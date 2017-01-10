d290ac2b73cfcfdc06c0efa2a7d610f0a80deb65
Leonardo Araujo <leonardo.araujo@apical.co.uk>
2017-01-10 17:18:26 +0000

    Adding code to measure time

--- src/armadillo_simple.cpp
+++ src/armadillo_simple.cpp
@@ -5,6 +5,7 @@ sudo apt-get install libarmadillo-dev libarmadillo4 r-cran-rcpparmadillo
 */
 #include <iostream>
 #include <armadillo>
+#include <chrono>
 
 using namespace std;
 using namespace arma;
@@ -13,19 +14,28 @@ int main() {
 
   // Create a A[3,2]
   mat A = randu<mat>(3,2);
+  //mat A = randu<mat>(1000,1000);
   // Create a A[2,3]
   mat B = randu<mat>(2,3);
+  //mat B = randu<mat>(1000,1000);
 
   cout << "\n\nMatrix A" << endl;
-  cout << A;
+  //cout << A;
 
   cout << "\n\nMatrix B" << endl;
-  cout << B;
+  //cout << B;
+
+  auto start = chrono::steady_clock::now();
+  mat C = A*B;
+  auto end = chrono::steady_clock::now();
+  auto diff = end - start;
+
+  cout << endl << "Execution time:"<< chrono::duration <double, milli> (diff).count() << " ms" << endl;
 
   // Do matrix multiplication
   cout << "\nResult:" << endl;
   //cout << A*B.t() << endl;
-  cout << A*B << endl;
+  cout << C << endl;
 
   return 0;
 }
