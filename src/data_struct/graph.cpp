4b5c24ad33c199ad233877297b481af0232ad476
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2017-02-07 09:53:28 +0000

    Update graph.cpp
    Some changes on graph example using a map with the keys been the nodes and the values a list

--- src/data_struct/graph.cpp
+++ src/data_struct/graph.cpp
@@ -1,8 +1,13 @@
+/*
+ * http://www.geeksforgeeks.org/graph-and-its-representations/
+ * http://www.geeksforgeeks.org/graph-implementation-using-stl-for-competitive-programming-set-1-dfs-of-unweighted-and-undirected/
+ * http://www.geeksforgeeks.org/depth-first-traversal-for-a-graph/
+ * */
 #include <iostream>
 #include <vector>
-#include <iterator>
 #include <list>
 #include <memory>
+#include <map>
 
 using namespace std;
 
@@ -15,35 +20,57 @@ public:
 template <class someType>
 class Edge{
 public:
-	unique_ptr<Vertice<someType>> m_Vertice = nullptr;
+	shared_ptr<Vertice<someType>> m_Vertice = nullptr;
 	int m_weight;
 };
 
 template <class someType>
 class Graph {
 private:
-	list<std::unique_ptr<Vertice<someType>>> m_verticeList;
+	// We will represent the graph with a map of
+	// Nodes and list of vertices
+	map<shared_ptr<Vertice<someType>>, shared_ptr<list<Edge<someType>>>> m_mapGraph;
 public:
-	void addVertice(int value) {
-		std::unique_ptr<Vertice<someType>> vertInst(new Vertice<someType>{value});
-		m_verticeList.push_back(move(vertInst));
+	shared_ptr<Vertice<someType>> addVertice(someType value) {
+		// Create a vertice instance
+		shared_ptr<Vertice<someType>> vertInst(new Vertice<someType>{value});
+		shared_ptr<list<Edge<someType>>> listEdge(new list<Edge<someType>>);
+		m_mapGraph[vertInst] = listEdge;
+		return vertInst;
 	}
 
-	void addEdge(const someType &v1, const someType &v2){
-		// Non directional graph
-		//m_verticeList[v1].push_back(v2);
-		//m_verticeList[v2].push_back(v1);
+	void addEdge(const shared_ptr<Vertice<someType>> &v1, const shared_ptr<Vertice<someType>> &v2){
+		// Get list reference for v1 and v2
+		shared_ptr<list<Edge<someType>>> v1List = m_mapGraph[v1];
+		shared_ptr<list<Edge<someType>>> v2List = m_mapGraph[v2];
+
+		// Connect edges (Non directional)
+		v1List->push_back(Edge<someType>{v2,0});
+		v2List->push_back(Edge<someType>{v1,0});
 	}
 };
 
 int main(){
 	Graph<int> graph;
-	graph.addVertice(0);
-	graph.addVertice(1);
-	graph.addVertice(2);
-	graph.addVertice(3);
-	graph.addVertice(4);
-	graph.addVertice(5);
+
+	// Add vertices
+	auto v0 = graph.addVertice(0);
+	auto v1 = graph.addVertice(1);
+	auto v2 = graph.addVertice(2);
+	auto v3 = graph.addVertice(3);
+	auto v4 = graph.addVertice(4);
+	auto v5 = graph.addVertice(5);
+
+	// Add edges (7 edges)
+	graph.addEdge(v0,v1);
+	graph.addEdge(v0,v4);
+	graph.addEdge(v1,v4);
+	graph.addEdge(v1,v3);
+	graph.addEdge(v1,v2);
+	graph.addEdge(v2,v3);
+	graph.addEdge(v3,v4);
+
+	cout << "End" << endl;
 
 	return 0;
 }
