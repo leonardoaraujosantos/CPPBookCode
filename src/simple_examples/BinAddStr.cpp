595b4072493b5e7ee89a97734658b28e87f565d9
Leonardo Araujo dos Santos <leonardoaraujo.santos@gmail.com>
2017-03-27 11:44:28 +0100

    Update BinAddStr.cpp
    Another solution on Java
    https://gist.github.com/zac-xin/4381455
    
    Peng solution
    ``` matlab
     function out_string = leoleo(stringA, stringB)
     limit_len = 3;
     
     stringA = char(stringA);
     stringB = char(stringB);
     
     
     max_len = max(size(stringA,2),size(stringB,2));
     if size(stringA,2)>size(stringB,2); stringB = cat(2,repmat('0',[1,size(stringA,2)-size(stringB,2)]),stringB);
     else stringA = cat(2,repmat('0',[1,size(stringB,2)-size(stringA,2)]),stringA);
     end
     
     m_loop = ceil(max_len/limit_len);
     temp_string{m_loop} = [];
     flag = 0;
     
     for i = m_loop:-1:1
        if i~=1
           temp_string{i} = dec2bin(bin2dec(stringA(end-(m_loop-i+1)*limit_len+1:end-(m_loop-i)*limit_len)) +...
                               bin2dec(stringB(end-(m_loop-i+1)*limit_len+1:end-(m_loop-i)*limit_len)));
           if flag
               temp_string{i} = dec2bin(bin2dec(temp_string{i}) + bin2dec('1'));
           end
           if size(temp_string{i},2) > limit_len
               flag = 1;
               temp_string{i} = temp_string{i}(2:end);
           else
               temp_string{i} = [repmat('0',[1,limit_len-size(temp_string{i},2)]),temp_string{i}];
               flag = 0;
           end
           
        else
            temp_string{i} = dec2bin(bin2dec(stringA(1:end-(m_loop-i)*limit_len)) +...
                               bin2dec(stringB(1:end-(m_loop-i)*limit_len)));
           if flag
               temp_string{i} = dec2bin(bin2dec(temp_string{i}) + bin2dec('1'));
           end
            
            
        end
       
     end
    
     out_string = [];
     for i = 1:m_loop
         out_string = [out_string, temp_string{i}];
     end
    
    %%test
    if strcmp(dec2bin(bin2dec(stringA) + bin2dec(stringB)),out_string)
        disp('pass');
    end
    ```

--- src/simple_examples/BinAddStr.cpp
+++ src/simple_examples/BinAddStr.cpp
@@ -9,6 +9,18 @@ inline int binChar2Num(const char ch){
     return val;
 }
 
+// Alexey inline func
+inline int getVal(const std::string a, size_t ind)
+{
+    if (a.size() <= ind)
+        return 0;
+
+    if (a[a.size() - ind - 1] == '1')
+        return 1;
+    else
+        return 0;
+}
+
 /*
     Add 2 binary numbers defined as strings "101" + "11"=1000.
     Attention: The strings can be really big and will not fit on a long long variable
@@ -65,6 +77,44 @@ string strBinSum(const string &num1, const string &num2){
         *itrResp = to_string(lastVal).at(0);
     }
     return resp;
+}
+
+// Alexey solution was coller than mine on the carry part, but he did not pre-allocate the result string
+string binary_string_sum(const string& a, const string& b){
+    // do some stupid checks that the number is correct and stuff like that
+    // it is possible to use string initially not to reconvert in the end; but this will require some more complex calculations
+    list<char> sum;
+
+    int maxBits = std::max(a.size(), b.size());
+
+    int val3 = 0;
+
+    for (int i = 0; i < maxBits; ++i)
+    {
+        // there is a possible optimisation - at some point we can just copy part of number, in case of '1010010101010101' + '1' like numbers
+        int res = getVal(a, i) + getVal(b, i) + val3;        
+
+        val3 = (res >= 2);
+
+        if (res % 2 == 0)
+            sum.push_front('0');
+        else
+            sum.push_front('1');
+    }
+
+    if(val3 == 1)
+        sum.push_front('1');
+
+    int numElements = sum.size();
+    string answer;
+
+    int i = 0;
+    for (const auto& it : sum)
+    {
+        answer.push_back(it);
+    }
+
+    return answer;
 }
 
 int main()
